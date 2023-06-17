20aecdec170c10dfcd2ca573315f096bbba045f9
Faisalshouman <126206270+Faisalshouman@users.noreply.github.com>
2023-06-18 00:49:13 +0300

    modelscommit

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+
+class Booking(models.Model) :
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(default=6)
+    booking_date = models.DateTimeField(default= datetime.now)
+
+    def __str__(self):
+        return self.Name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.title
+    
